f78aa0b9cb8d9e558441223f799451913a473c58
Yevhen-L <ujeen2101@gmail.com>
2023-10-30 10:05:50 +0200

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -9,12 +9,25 @@ const persistConfig = {
   whitelist: ['contacts'],
 };
 
+const loggerMiddleware = store => next => action => {
+  store.getState(action);
+
+  next(action);
+  store.getState();
+};
+
 const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
 
 const store = configureStore({
   reducer: {
     contacts: persistedContactsReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: ['persist/PERSIST'],
+      },
+    }).concat(loggerMiddleware),
 });
 
 export const persistor = persistStore(store);
